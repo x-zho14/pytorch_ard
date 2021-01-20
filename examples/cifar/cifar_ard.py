25525c8fc729ac44504564bdd751fc874c9c2c9e
x-zho14ls <18813119938@163.com>
2021-01-20 22:00:13 +0800

    Update cifar_ard.py

--- examples/cifar/cifar_ard.py
+++ examples/cifar/cifar_ard.py
@@ -143,4 +143,8 @@ def test(epoch):
 
 for epoch in range(start_epoch, start_epoch + n_epoches):
     train(epoch)
+    for n, m in model.named_modules():
+        if hasattr(m, "log_sigma2"):
+            print(f"==> print {n} 's log_sigma2 grad magnitude'")
+            print(m.log_sigma2.grad)
     test(epoch)
