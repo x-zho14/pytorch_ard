66a12f70ba94273b4d33ee8ce36a9ef4c68f374b
x-zho14ls <18813119938@163.com>
2021-01-22 18:33:50 +0800

    Update models.py

--- examples/models.py
+++ examples/models.py
@@ -117,6 +117,9 @@ class VGG(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
+        print(x.size())
+        x = x.view(x.size(0), -1)
+        print(x.size())
         x = self.linear(x)
         return x.squeeze()
 
