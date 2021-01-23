88aa8aca1b421aa494458e71fda7bc9c3399c777
x-zho14ls <18813119938@163.com>
2021-01-23 15:24:04 +0800

    Update cifar_ard.py

--- examples/cifar/cifar_ard.py
+++ examples/cifar/cifar_ard.py
@@ -168,55 +168,55 @@ for epoch in range(start_epoch, start_epoch + n_epoches):
     train(epoch)
     log_list = []
     # log_layer_dict = {}
-    if epoch == 0:
-        idx = 0
-        import matplotlib.pyplot as plt
-        from matplotlib.ticker import PercentFormatter
+    # if epoch == 0:
+    idx = 0
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import PercentFormatter
 
-        total = 0
-        sum = 0
-        for n, m in model.named_modules():
-            if hasattr(m, "log_sigma2"):
-                # idx += 1
-                # print(f"==> flattening {n} subnet")
-                log_flattened = torch.flatten(torch.log10(torch.abs(m.log_sigma2.grad.data)+1e-20))
-                # print("fraction of zero grad:", float(torch.sum(m.log_sigma2.grad.data==0).item())/log_flattened.nelement())
-                # val_flattened = torch.flatten(m.log_sigma2.data)
-                # print("fraction of non -10:", float(torch.sum(val_flattened != -10).item()) / val_flattened.nelement())
-                sum += torch.sum(m.log_sigma2.grad==0).item()
-                total += m.log_sigma2.nelement()
-                # log_layer_dict[idx] = log_flattened.tolist()
-                plt.hist(log_flattened.tolist(), weights=np.ones(len(log_flattened)) / len(log_flattened),  bins=50)
-                # plt.xlim(0, 1)
-                plt.xlabel("Magnitude of Gradient (log scale)")
-                plt.ylabel("Percentage")
-                plt.title("Histogram of Gradient of Sparse VD")
-                plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-                plt.grid(True, linestyle="--")
-                plt.savefig("VGG"+str(idx)+"epoch"+str(epoch)+".pdf", bbox_inches='tight')
-                plt.clf()
-                plt.cla()
-                log_list.extend(log_flattened.tolist())
-                break
-        print("Whole fraction of zero grad:", float(sum)/float(total))
-        frac_list.append(float(sum)/float(total))
-        # print("check, ", len(log_list)- np.count_nonzero(log_list), sum)
-        # plt.hist(log_list, bins=50, density=True)
-        # print("before hist")
-        plt.hist(log_list, weights=np.ones(len(log_list)) / len(log_list), bins=50)
-        # print("after hist")
-        plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-        # plt.show()
-        # import pickle
-        # f = open('30_log_list.pickle', 'w')
-        # pickle.dump(log_list, f)
-        # f.close()
-        # plt.xlim(0, 1)
-        plt.xlabel("Magnitude of Gradient (log scale)")
-        plt.ylabel("Percentage")
-        plt.title("Histogram of Grad of Sparse VD")
-        plt.grid(True, linestyle="--")
-        plt.savefig("VGG"+"_"+'whole'+str(epoch)+".pdf", bbox_inches='tight')
+    total = 0
+    sum = 0
+    for n, m in model.named_modules():
+        if hasattr(m, "log_sigma2"):
+            # idx += 1
+            # print(f"==> flattening {n} subnet")
+            log_flattened = torch.flatten(torch.log10(torch.abs(m.log_sigma2.grad.data)+1e-20))
+            # print("fraction of zero grad:", float(torch.sum(m.log_sigma2.grad.data==0).item())/log_flattened.nelement())
+            # val_flattened = torch.flatten(m.log_sigma2.data)
+            # print("fraction of non -10:", float(torch.sum(val_flattened != -10).item()) / val_flattened.nelement())
+            sum += torch.sum(m.log_sigma2.grad==0).item()
+            total += m.log_sigma2.nelement()
+            # log_layer_dict[idx] = log_flattened.tolist()
+            plt.hist(log_flattened.tolist(), weights=np.ones(len(log_flattened)) / len(log_flattened),  bins=50)
+            # plt.xlim(0, 1)
+            plt.xlabel("Magnitude of Gradient (log scale)")
+            plt.ylabel("Percentage")
+            plt.title("Histogram of Gradient of Sparse VD")
+            plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+            plt.grid(True, linestyle="--")
+            plt.savefig("VGG"+str(idx)+"epoch"+str(epoch)+".pdf", bbox_inches='tight')
+            plt.clf()
+            plt.cla()
+            log_list.extend(log_flattened.tolist())
+            break
+    print("Whole fraction of zero grad:", float(sum)/float(total))
+    frac_list.append(float(sum)/float(total))
+    # print("check, ", len(log_list)- np.count_nonzero(log_list), sum)
+    # plt.hist(log_list, bins=50, density=True)
+    # print("before hist")
+    plt.hist(log_list, weights=np.ones(len(log_list)) / len(log_list), bins=50)
+    # print("after hist")
+    plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+    # plt.show()
+    # import pickle
+    # f = open('30_log_list.pickle', 'w')
+    # pickle.dump(log_list, f)
+    # f.close()
+    # plt.xlim(0, 1)
+    plt.xlabel("Magnitude of Gradient (log scale)")
+    plt.ylabel("Percentage")
+    plt.title("Histogram of Grad of Sparse VD")
+    plt.grid(True, linestyle="--")
+    plt.savefig("VGG"+"_"+'whole'+str(epoch)+".pdf", bbox_inches='tight')
     # for n, m in model.named_modules():
     #     print(n, m)
     #     if hasattr(m, "log_sigma2"):
